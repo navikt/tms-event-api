9eaa94ccf8b3b7733ad4bffbf1f831cb3cde02f1
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-09-07 11:37:15 +0200

    fjern merge revert sak

--- src/main/kotlin/no/nav/tms/event/api/varsel/VarselReader.kt
+++ src/main/kotlin/no/nav/tms/event/api/varsel/VarselReader.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import no.nav.tms.event.api.config.AzureTokenFetcher
 import no.nav.tms.event.api.config.getWithAzureAndFnr
 import no.nav.tms.event.api.config.retryOnConnectionLost
-import org.slf4j.LoggerFactory
 import java.net.URL
 
 class VarselReader(
@@ -17,10 +16,6 @@ class VarselReader(
         varselPath: String
     ): List<VarselDTO> {
         val completePathToEndpoint = URL("$eventHandlerBaseURL/$varselPath")
-<<<<<<< Updated upstream
-        log.debug("Forsøker å hente  $varselPath")
-=======
->>>>>>> Stashed changes
         val azureToken = azureTokenFetcher.fetchTokenForEventHandler()
         return retryOnConnectionLost {
             client.getWithAzureAndFnr(completePathToEndpoint, azureToken, fnr)
